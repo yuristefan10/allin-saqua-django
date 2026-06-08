9bf5b027fd839d483c64a36074315943f4ce9a74
Yuri Vieira <yuri.vieira@example.com>
2026-06-08 17:20:35 -0300

    fix: separar collectstatic do startup, aumentar healthcheck timeout

--- allin_django/settings.py
+++ allin_django/settings.py
@@ -86,7 +86,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
