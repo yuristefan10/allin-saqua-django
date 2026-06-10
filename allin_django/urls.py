97979e820845e069bf25e76be9e1b9d9f827416c
Yuri Vieira <yuri.vieira@example.com>
2026-06-10 12:20:14 -0300

    Serve media files in production via django.views.static.serve

--- allin_django/urls.py
+++ allin_django/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
+import re
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('portal.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path(re.sub(r'^/', '', settings.MEDIA_URL) + '<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
+]
